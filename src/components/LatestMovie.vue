820435de80b2c415a1933c0780cc9d4a310e3bde
dannicada <dannynwangwu@gmail.com>
2019-07-29 04:25:07 +0100

    added axios and LatestMovie component

---
+++ src/components/LatestMovie.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        latest movie
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            wholeResponse: [],
+            loading: true,
+        }
+    },
+},
+mounted() {
+    axios
+        .get ('http://www.omdbapi.com/?s=mummy&apikey=XXXXX&page=1&type=movie&Content-Type=application/json')
+        .then (response => {
+            this.wholeResponse = response.data.Search
+        })
+        .catch(error => {
+            console.log(error)
+        })
+},
+</script>
